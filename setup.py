c142f114453336de616461a7ea8730e3eeb98de2
vivekalex61 <vivekalex61@gmail.com>
2021-08-16 13:27:11 +0530

    third commit

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'insightsearch',         # How you named your package folder (MyLib)
   packages = ['insightsearch'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Insightsearch will show hidden sentiments, aspects and their opinions from your text data. It will show what people are talking about your product/service',   # Give a short description about your library
   author = 'Vivek alex',                   # Type in your name
   author_email = 'vivekalexktr@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/vivekalex61/insightsearch/archive/refs/tags/v0.2.tar.gz',  
+  url = 'https://github.com/vivekalex61/insightsearch',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/vivekalex61/insightsearch/archive/refs/tags/v0.3.tar.gz',  
   keywords = ['NLP', 'INSIGHT', 'REVIEW ANALYZER','REVIEW'],   # Keywords that define your package best
   project_urls={
          
@@ -19,7 +19,7 @@ setup(
           'transformers',
           'spacy',
           'pandas',
-      'nltk','plotly','textblob','os','pathlib','tensorflow'
+      'nltk','plotly','textblob','tensorflow'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
